17b63fdb77744560778894ea20e0e62ef8130b7d
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-22 15:54:22 +0200

    Add project files.

---
+++ ExercisePetle4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ExercisePetle4
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            //Numer read
+            Console.WriteLine("Podaj liczbę");
+            Int32.TryParse(Console.ReadLine(), out a);
+
+            //Calculation
+            while(a >= b)
+            {
+                for (int i = 0; i <= c; i++)
+                {
+                    if (a >= b)
+                    {
+                        if (i == 0)
+                        {
+                            Console.WriteLine(b);
+                            b++;
+                        }
+                        else
+                        {
+                            Console.Write(b);
+                            b++;
+                        }
+                    }
+                }
+                c++;
+            }
+        }
+            
+        
+            
+            
+            
+        
+    }
+}
